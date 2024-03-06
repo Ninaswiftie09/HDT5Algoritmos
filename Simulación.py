6b3107947325c7317de5f252894ef7fe328bcda7
Ninaswiftie09 <131787063+Ninaswiftie09@users.noreply.github.com>
2024-03-05 23:12:38 -0600

    Add files via upload

---
+++ Simulación.py
@@ -0,0 +1,37 @@
+#Nina Nájera 231088 
+#Hoja de Trabajo 5 Algoritmos
+
+#Importamos las librerias
+import simpy
+import random
+import matplotlib.pyplot as plt
+
+#Se definen las constantes que dicen en las instrucciones
+SEED = 42
+RAM = 100
+PROCESOS = [25, 50, 100, 150, 200]
+INTERVALOS = [10, 5, 1]
+TIEMPO_SIMULACION = 1000
+
+promedio_de_tiempos = []
+
+#Se crea la clase que simulará el sistema operativo
+class Sistema:
+    def __init__(self, entorno, cantidad_proceso, intervalo):
+        self.entorno = entorno
+        self.cpu = simpy.Resource(entorno, capacity=1)
+        self.memoria = simpy.Container(entorno, capacity=RAM, init=RAM)
+        self.cantidad_proceso = cantidad_proceso
+        self.intervalo = intervalo
+        self.tiempos_procesos = []
+
+    def proceso(self, nombre, ver):
+        llegada = self.entorno.now
+        yield self.memoria.get(ver)
+        with self.cpu.request() as req:
+            yield req
+            while ver > 0:
+                yield self.entorno.timeout(random.expovariate(1))
+                ver -= 1
+            self.tiempos_procesos.append(self.entorno.now - llegada)
+
